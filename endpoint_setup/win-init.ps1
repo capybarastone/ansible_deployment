10ab70294adf5037fa3eb9403c09411fd3560a8c
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2026-02-15 17:35:12 -0500

    Create win-init.ps1

---
+++ endpoint_setup/win-init.ps1
@@ -0,0 +1,11 @@
+## Script used to intitialize test environment setup on windows
+# To Use: As Admin do - "wget https://raw.githubusercontent.com/capybarastone/ansible_deployment/refs/heads/main/endpoint_setup/win-init.ps1", then ".\win-init.ps1"
+
+# Install OpenSSH Server for Windows host
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+
+# Set PowerShell to be default shell on Windows for ansible
+Set-ItemProperty "HKLM:\Software\Microsoft\Powershell\1\ShellIds" -Name ConsolePrompting -Value $true
+New-ItemProperty -Path HKLM:\SOFTWARE\OpenSSH -Name DefaultShell -Value "C\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
