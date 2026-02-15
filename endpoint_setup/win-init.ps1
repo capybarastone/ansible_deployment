62866d880aa9cfba214de840820612b941c86b87
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2026-02-15 18:00:36 -0500

    Update win-init.ps1

--- endpoint_setup/win-init.ps1
+++ endpoint_setup/win-init.ps1
@@ -8,4 +8,4 @@ Set-Service -Name sshd -StartupType 'Automatic'
 
 # Set PowerShell to be default shell on Windows for ansible
 Set-ItemProperty "HKLM:\Software\Microsoft\Powershell\1\ShellIds" -Name ConsolePrompting -Value $true
-New-ItemProperty -Path HKLM:\SOFTWARE\OpenSSH -Name DefaultShell -Value "C\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+New-ItemProperty -Path HKLM:\SOFTWARE\OpenSSH -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
