f7f176245e2ff99d263b94a4fd7748ba3db78fad
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2026-02-15 17:36:55 -0500

    Create deb-init.bash

---
+++ endpoint_setup/deb-init.bash
@@ -0,0 +1,8 @@
+## Script used to intitialize test environment setup on windows
+# To Use: "wget https://raw.githubusercontent.com/capybarastone/ansible_deployment/refs/heads/main/endpoint-setup/deb-init.bash", then "sudo bash deb-init.bash"
+
+# Create Deployment User on mgmt
+read -p "enter deployment user password: " Pass
+hashPass=$(openssl passwd -1 $Pass)
+sudo useradd -m deployment -p $hashPass -s /bin/bash
+sudo usermod -aG sudo deployment
